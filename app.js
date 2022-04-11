69ac56d743481f2f42a1d5673389c9e383b8f544
Naman Dadhich <namankdadhich001@gmail.com>
2022-04-11 15:03:43 +0530

    CSS and Mongo Final changes

--- app.js
+++ app.js
@@ -1,55 +1,67 @@
-require ('dotenv').config();
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-const mongoose = require('mongoose')
-const seed = require('./seed')
-const {post_book, post_magazine, bm, bmsort, bm_isbn, bma} = require('./controllers/controllers')
+const dotenv = require("dotenv");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const seed = require("./seed");
+const {
+  post_book,
+  post_magazine,
+  bm,
+  bmsort,
+  bm_isbn,
+  bma,
+} = require("./controllers/controllers");
 
+dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-    console.log('database connected');
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("database connected");
     // remove seed when done loading once
     seed();
-}).catch(err => console.error(err));
+  })
+  .catch((err) => console.error(err));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.set('view engine', 'ejs');
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 // routes
 
 // get main page
-app.get('/', (req, res) => {
-    res.render('main');
-})
+app.get("/", (req, res) => {
+  res.render("main");
+});
 
 // form to add new book or a magazine
-app.get('/add', (req, res) => {
-    res.render('book-form')
-})
+app.get("/add", (req, res) => {
+  res.render("book-form");
+});
 
 // post new book and export csv
-app.post('/book', post_book)
+app.post("/book", post_book);
 
 // post new magazines and export csv
-app.post('/magazine', post_magazine)
+app.post("/magazine", post_magazine);
 
 // get book or magazine with isbn
-app.get('/bmi/:isbn', bm_isbn)
+app.get("/bmi/:isbn", bm_isbn);
 
 // get all magazines and books with details
-app.get('/bm', bm)
+app.get("/bm", bm);
 
 // get all magazines and books with details sorted by title
-app.get('/bmsort', bmsort)
+app.get("/bmsort", bmsort);
 
 // get all magazines and books with a specific author
-app.get('/bma/:email', bma)
+app.get("/bma/:email", bma);
 
-app.get("*", (req, res) => { res.redirect('/') })
+app.get("*", (req, res) => {
+  res.redirect("/");
+});
 
 app.listen(PORT, () => {
-    console.log('listening on port ' + PORT)
-})
+  console.log("listening on port " + PORT);
+});
